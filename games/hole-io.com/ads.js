a2d658012bea80d458e9ae603622ee2552084500
_monkeytime3_ <102395345+monkey3three@users.noreply.github.com>
2023-10-02 17:11:56 +1100

    more games
    6 new games

--- games/hole-io.com/ads.js
+++
@@ -1,1 +0,0 @@
-var NoAdblock = 1;
--- games/hyperspace-master/hyperspace-master/server/ai.go
+++
@@ -1,7 +0,0 @@
-package main
-
-// This doesn't do anything yet, but I (icco) would like it to create a new
-// client that spins in circles or something.
-func AddAi() error {
-	return nil
-}
